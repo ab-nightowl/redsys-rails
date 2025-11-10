542eee1c635d015eaf07af3d78513c1f0afade34
Alexandre Brocard <a.brocard@ntymail.com>
2025-11-10 14:59:15 +0100

    fix: add skip_before_action :authenticate_user! in TpvController

--- app/controllers/redsys/tpv_controller.rb
+++ app/controllers/redsys/tpv_controller.rb
@@ -1,6 +1,7 @@
 module Redsys
   class TpvController < ApplicationController
     # skip_before_action :verify_authenticity_token, only: [:confirmation]
+    skip_before_action :authenticate_user!
 
     #
     # Formulario de salto a la pasarela de pago
